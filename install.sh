376dee8399a0e5a2bee76bb5d561030e468e74c4
jekky <11986158+jac3km4@users.noreply.github.com>
2022-11-18 23:40:03 +0000

    fixing javadoc

--- install.sh
+++ install.sh
@@ -6,3 +6,4 @@ cd "ghidra_${GHIDRA_VERSION}_PUBLIC"
 support/buildGhidraJar
 
 mvn install:install-file -DgroupId=$GROUP -DartifactId=ghidra -Dpackaging=jar -Dversion=$VERSION -Dfile=ghidra.jar -DgeneratePom=true
+cp "./staging-docs/api/javadoc.jar" "$HOME/.m2/repository/${GROUP//.//}/ghidra/${VERSION}/ghidra-${VERSION}-javadoc.jar"
--- setup.sh
+++ setup.sh
@@ -5,3 +5,9 @@ set -o pipefail
 curl "https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_${GHIDRA_VERSION}_build/ghidra_${GHIDRA_VERSION}_PUBLIC_${GHIDRA_REVISION}.zip" \
     -L -o ghidra.zip
 unzip ghidra.zip
+
+cd "./ghidra_${GHIDRA_VERSION}_PUBLIC"
+unzip "./docs/GhidraAPI_javadoc.zip" -d ./staging-docs
+
+cd ./staging-docs/api
+zip "javadoc.jar" *
