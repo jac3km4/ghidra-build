3041a121a119697160f491639e9340c8ca24b847
jekky <11986158+jac3km4@users.noreply.github.com>
2022-11-18 22:19:53 +0000

    Use provided version

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ set -o pipefail
 cd "ghidra_${GHIDRA_VERSION}_PUBLIC"
 support/buildGhidraJar
 
-mvn install:install-file -DgroupId=$GROUP -DartifactId=ghidra -Dpackaging=jar -Dversion=$GHIDRA_VERSION -Dfile=ghidra.jar -DgeneratePom=true
+mvn install:install-file -DgroupId=$GROUP -DartifactId=ghidra -Dpackaging=jar -Dversion=$VERSION -Dfile=ghidra.jar -DgeneratePom=true
